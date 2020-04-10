8019b9ed73a26f39d708d0f36d8e284bf8f1a3da
GuilhermeAbreu <Guilherme.abreu2019@gmail.com>
2020-04-09 22:15:17 -0300

    um teste de commit

--- src/main/java/br/com/devshangman/JogoDaForca/JogoDaForca.java
+++ src/main/java/br/com/devshangman/JogoDaForca/JogoDaForca.java
@@ -40,6 +40,7 @@ public class JogoDaForca {
             Jogador nomeJogador = jogadores.get(0);
 
             for(Jogador anObject : jogadores){
+                //aqui nao pode ser apagdo
                 System.out.println("aqui e um teste");
                 System.out.println(anObject);
             }
